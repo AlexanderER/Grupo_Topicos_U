a5804b7df2049d5fb545173717586bcbd8951410
Alexander <alexer1515@gmail.com>
2016-10-17 10:39:38 -0600

    Creacion de estructura Base para el Proyecto

---
+++ Bingo System/Bingo System/clsGlobal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bingo_System
+{
+    class clsGlobal
+    {
+        #region Variables Globales de Configuracion del Bingo
+
+        public static Int32 iLimiteNumerosBingo = 25;   // Se inicializa en 25 ya que es el numero minimo.
+
+        #endregion
+    }
+}
---
+++ Bingo System/LogicaNegocio/Logica.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio
+{
+    public class Logica
+    {
+        #region Variables
+
+        /// <summary>
+        /// ENUM que contiene las modalidades de Juego del Bingo
+        /// </summary>
+        public enum MODALIDAD_JUEGO
+        {
+            CARTON_LLENO = 0,
+            CUATRO_ESQUINAS = 1,
+            LETRA_H = 2,
+            LETRA_X = 3,
+            LETRA_O = 4,
+            LETRA_U = 5,
+            LETRA_P = 6,
+            LETRA_A = 7,
+            LETRA_E = 8,
+            LETRA_W = 9,
+            LETRA_R = 10
+        }
+
+        #endregion
+    }
+}
