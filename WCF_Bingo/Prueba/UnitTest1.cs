11e1dec10788553a0b090f50bab739fd8612de08
dennis0795 <dennis07_09@hotmail.com>
2016-10-20 20:00:49 -0600

    Creacion  de la solucion de Prueba, Creacion de primer metodo de prueba

---
+++ WCF_Bingo/Prueba/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+
+namespace Prueba
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Int32 numeroTecho = 5;
+
+            Int32[] lista =new Int32[] { 1, 2, 3 };
+            
+            Int32 numeroEsperado = 4;
+
+
+            ServiceReference1.Service1Client servicio = new ServiceReference1.Service1Client();
+            Int32 resultado = servicio.GenerarNumero(numeroTecho, lista);
+
+            Assert.AreEqual(resultado, numeroEsperado);
+
+        }
+    }
+}
