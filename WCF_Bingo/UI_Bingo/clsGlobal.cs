4720b1f514acaff6c27ad47b122875cb5cd9f177
Alexander <alexer1515@gmail.com>
2016-10-20 20:27:12 -0600

    Creación de clase Global

---
+++ WCF_Bingo/UI_Bingo/clsGlobal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI_Bingo
+{
+    class clsGlobal
+    {
+
+        #region Variables Globales de Configuracion del Bingo
+
+        // Listado de Numeros Favorecidos (Son los numeros que ya han salido en el sorteo)
+        public static List<Int32> ListaNumerosFavorecidos;
+
+        #endregion
+
+    }
+}
