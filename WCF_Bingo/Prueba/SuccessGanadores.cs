1528e1e662d9d00ea6600fd739e791d82cd2a19c
alonsobugs <alonsobugs@gmail.com>
2016-10-22 12:07:16 -0600

    ok

--- WCF_Bingo/Prueba/SuccessGanadores.cs
+++ WCF_Bingo/Prueba/SuccessGanadores.cs
@@ -46,49 +46,7 @@ namespace Prueba
             Assert.AreEqual(ganadores.Count, 1);
         }
 
-        //addicional
-
-
-        [TestMethod]
-        public void Ganadores2()
-        {
-            List<WCF_Bingo.Clases.clsJugador> listaJugadores = new List<WCF_Bingo.Clases.clsJugador>();
-
-            int nombreUsuario = new int();
-            int cantidadCartones = new int();
-            int cantidadNumerosAJugar = new int();
-            int modalidad = new int();
-
-            // listaJugadores.Add(jugador);
-
-            nombreUsuario = 1;
-            cantidadCartones = 1;
-            cantidadNumerosAJugar = 25;
-            modalidad = 1;
-
-            // int numeroSalido = new int();
-            // numeroSalido = 1;
-            ServiceReference1.Service1Client servicio = new ServiceReference1.Service1Client();
-            List<WCF_Bingo.Clases.clsJugador> resultado = servicio.crearJugador(listaJugadores, nombreUsuario, cantidadCartones, cantidadNumerosAJugar, modalidad);
-
-            int res = resultado[0].ListaCartones[0].NumeroAReducirParaGanar;
-
-            List<WCF_Bingo.Clases.clsJugador> ganadores = new List<WCF_Bingo.Clases.clsJugador>();
-
-            List<WCF_Bingo.Clases.clsJugador> re = new List<WCF_Bingo.Clases.clsJugador>();
-            for (int p = 1; p <= 25; p++)
-            {
-                resultado = servicio.actualizarEstados(resultado, p);
-                ganadores = servicio.jugadoresGanadores(resultado);
-                if (ganadores.Count >= 1)
-                {
-                    break;
-                }
-            }
-
-            Assert.AreEqual(ganadores.Count, 1);
-        }
-
+        
 
 
     }
