b4edd587698c2517a09fe22037a9dfb5b56842c2
Eddy Segura <eseguran@outlook.com>
2016-10-22 10:37:42 -0600

    cambio colores

--- WCF_Bingo/UI_Bingo/Form4.cs
+++ WCF_Bingo/UI_Bingo/Form4.cs
@@ -67,28 +67,118 @@ namespace UI_Bingo
             WCF_Bingo.Clases.clsCarton carton = new WCF_Bingo.Clases.clsCarton();
             carton=  WCF_Service.consultarCarton(clsGlobal.ListaJugador, Convert.ToInt32(cmbNumJugador.Text), Convert.ToInt32(cmbNumCarton.Text)-1);
             temp = carton.Columnas[0];
-            lbl00.Text = Convert.ToString(temp[0].Numero);
-            lbl01.Text = Convert.ToString(temp[1].Numero);
-            lbl02.Text = Convert.ToString(temp[2].Numero);
-            lbl03.Text = Convert.ToString(temp[3].Numero);
-            lbl04.Text = Convert.ToString(temp[4].Numero);
+            if (temp[0].AJugar == true) { lbl00.Text = Convert.ToString(temp[0].Numero); }
+            else {
+                this.lbl00.BackColor = System.Drawing.Color.MidnightBlue;
+                this.lbl00.ForeColor = System.Drawing.Color.White;
+                lbl00.Text = Convert.ToString(temp[0].Numero);}
+
+            if (temp[1].AJugar == true) { lbl01.Text = Convert.ToString(temp[1].Numero); }
+            else
+            {
+                this.lbl01.BackColor = System.Drawing.Color.MidnightBlue;
+                this.lbl01.ForeColor = System.Drawing.Color.White;
+                lbl01.Text = Convert.ToString(temp[1].Numero);
+            }
+
+            if (temp[2].AJugar == true) { lbl02.Text = Convert.ToString(temp[2].Numero); }
+            else
+            {
+                this.lbl02.BackColor = System.Drawing.Color.MidnightBlue;
+                this.lbl02.ForeColor = System.Drawing.Color.White;
+                lbl02.Text = Convert.ToString(temp[2].Numero);
+            }
+            if (temp[3].AJugar == true) { lbl03.Text = Convert.ToString(temp[3].Numero); }
+            else
+            {
+                this.lbl03.BackColor = System.Drawing.Color.MidnightBlue;
+                this.lbl03.ForeColor = System.Drawing.Color.White;
+                lbl03.Text = Convert.ToString(temp[3].Numero);
+            }
+            if (temp[4].AJugar == true) { lbl04.Text = Convert.ToString(temp[4].Numero); }
+            else
+            {
+                this.lbl04.BackColor = System.Drawing.Color.MidnightBlue;
+                this.lbl04.ForeColor = System.Drawing.Color.White;
+                lbl04.Text = Convert.ToString(temp[4].Numero);
+            }
 
             temp = carton.Columnas[1];
-            lbl10.Text = Convert.ToString(temp[0].Numero);
-            lbl11.Text = Convert.ToString(temp[1].Numero);
-            lbl12.Text = Convert.ToString(temp[2].Numero);
-            lbl13.Text = Convert.ToString(temp[3].Numero);
-            lbl14.Text = Convert.ToString(temp[4].Numero);
+            if (temp[0].AJugar == true) { lbl10.Text = Convert.ToString(temp[0].Numero); }
+            else
+            {
+                this.lbl10.BackColor = System.Drawing.Color.MidnightBlue;
+                this.lbl10.ForeColor = System.Drawing.Color.White;
+                lbl10.Text = Convert.ToString(temp[0].Numero);
+            }
+            if (temp[1].AJugar == true) { lbl11.Text = Convert.ToString(temp[1].Numero); }
+            else
+            {
+                this.lbl11.BackColor = System.Drawing.Color.MidnightBlue;
+                this.lbl11.ForeColor = System.Drawing.Color.White;
+                lbl11.Text = Convert.ToString(temp[1].Numero);
+            }
+            if (temp[2].AJugar == true) { lbl12.Text = Convert.ToString(temp[2].Numero); }
+            else
+            {
+                this.lbl12.BackColor = System.Drawing.Color.MidnightBlue;
+                this.lbl12.ForeColor = System.Drawing.Color.White;
+                lbl12.Text = Convert.ToString(temp[2].Numero);
+            }
+            if (temp[3].AJugar == true) { lbl13.Text = Convert.ToString(temp[3].Numero); }
+            else
+            {
+                this.lbl13.BackColor = System.Drawing.Color.MidnightBlue;
+                this.lbl13.ForeColor = System.Drawing.Color.White;
+                lbl13.Text = Convert.ToString(temp[3].Numero);
+            }
+            if (temp[4].AJugar == true) { lbl14.Text = Convert.ToString(temp[4].Numero); }
+            else
+            {
+                this.lbl14.BackColor = System.Drawing.Color.MidnightBlue;
+                this.lbl14.ForeColor = System.Drawing.Color.White;
+                lbl14.Text = Convert.ToString(temp[4].Numero);
+            }
 
             temp = carton.Columnas[2];
-            lbl20.Text = Convert.ToString(temp[0].Numero);
-            lbl21.Text = Convert.ToString(temp[1].Numero);
+            if (temp[0].AJugar == true) { lbl20.Text = Convert.ToString(temp[0].Numero); }
+            else
+            {
+                this.lbl20.BackColor = System.Drawing.Color.MidnightBlue;
+                this.lbl20.ForeColor = System.Drawing.Color.White;
+                lbl20.Text = Convert.ToString(temp[0].Numero);
+            }
+            if (temp[1].AJugar == true) { lbl21.Text = Convert.ToString(temp[1].Numero); }
+            else
+            {
+                this.lbl21.BackColor = System.Drawing.Color.MidnightBlue;
+                this.lbl21.ForeColor = System.Drawing.Color.White;
+                lbl21.Text = Convert.ToString(temp[1].Numero);
+            }
             //lbl22.Text = Convert.ToString(temp[2].Numero);
-            lbl23.Text = Convert.ToString(temp[3].Numero);
-            lbl24.Text = Convert.ToString(temp[4].Numero);
+            if (temp[3].AJugar == true) { lbl23.Text = Convert.ToString(temp[3].Numero); }
+            else
+            {
+                this.lbl23.BackColor = System.Drawing.Color.MidnightBlue;
+                this.lbl23.ForeColor = System.Drawing.Color.White;
+                lbl23.Text = Convert.ToString(temp[3].Numero);
+            }
+            if (temp[4].AJugar == true) { lbl24.Text = Convert.ToString(temp[4].Numero); }
+            else
+            {
+                this.lbl24.BackColor = System.Drawing.Color.MidnightBlue;
+                this.lbl24.ForeColor = System.Drawing.Color.White;
+                lbl24.Text = Convert.ToString(temp[4].Numero);
+            }
 
             temp = carton.Columnas[3];
-            lbl30.Text = Convert.ToString(temp[0].Numero);
+            if (temp[0].AJugar == true) { lbl30.Text = Convert.ToString(temp[0].Numero); }
+            else
+            {
+                this.lbl30.BackColor = System.Drawing.Color.MidnightBlue;
+                this.lbl30.ForeColor = System.Drawing.Color.White;
+                lbl30.Text = Convert.ToString(temp[0].Numero);
+            }
             lbl31.Text = Convert.ToString(temp[1].Numero);
             lbl32.Text = Convert.ToString(temp[2].Numero);
             lbl33.Text = Convert.ToString(temp[3].Numero);
