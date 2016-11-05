8f35a6f320fa1b8d688fe8f410d2c0d2a7970b1c
Alexander <alexer1515@gmail.com>
2016-11-05 10:31:19 -0600

    Base

---
+++ Ejemplo_EnterpriseLibrary/Ejemplo_EnterpriseLibrary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.EnterpriseLibrary.Logging;
+
+namespace Ejemplo_EnterpriseLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogEntry entry = new LogEntry();
+            entry.Message  = "Mensaje a registrar";
+            entry.Severity = System.Diagnostics.TraceEventType.Critical;
+            entry.Categories.Add("General");
+            Logger.Write(entry);
+        }
+    }
+}
