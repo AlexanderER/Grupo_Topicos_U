fa3d11439903e45f08305f30f8c9e5c514587779
Alexander <alexer1515@gmail.com>
2016-11-05 00:31:56 -0600

    Logs configurados en Consola

--- Ejemplo_Uso_NLog/Ejemplo_Uso_NLog/Program.cs
+++ Ejemplo_Uso_NLog/Ejemplo_Uso_NLog/Program.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLog;
 
 namespace Ejemplo_Uso_NLog
 {
     class Program
     {
+        //--------------------------------------------------------------------
+        private static Logger logger = LogManager.GetCurrentClassLogger();  //
+        //--------------------------------------------------------------------
+
         static void Main(string[] args)
         {
+            //--------------------------------------------------------------------
+            logger.Trace("Ejemplo log tipo 'Trace'");
+            logger.Debug("Ejemplo log tipo 'Debug'");
+            logger.Info("Ejemplo log tipo 'Info'");
+            logger.Warn("Ejemplo log tipo 'Warning'");
+            logger.Error("Ejemplo log tipo 'Error'");
+            logger.Fatal("Ejemplo log tipo 'Fatal'");
+
+            Console.ReadLine();
+            //--------------------------------------------------------------------
         }
     }
 }
