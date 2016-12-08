68cd34588178e54715adf7af146eee004470ca69
Alexander <alexer1515@gmail.com>
2016-12-07 20:06:34 -0600

    Nuevo Proyecto en la Solucion (Modulo_Peliculas_Model)

---
+++ Modulo_Peliculas/Modulo_Peliculas_Model/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulo_Peliculas_Model
+{
+    public class Class1
+    {
+    }
+}
